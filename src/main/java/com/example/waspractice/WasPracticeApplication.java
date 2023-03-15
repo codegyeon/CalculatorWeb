a2b7b2b055337b64c11847e2bab2c7443005f7ef
codegyeon <wjdrlgus313@naver.com>
2023-03-15 09:33:24 +0900

    first

---
+++ src/main/java/com/example/waspractice/WasPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.waspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/waspractice/WasPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
